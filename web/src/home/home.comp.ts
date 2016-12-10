db73facb56f91d4aff6b94e21a71fea6f41490be
nneff <nneff>
2016-12-10 17:47:51 -0600

    Added a user service. put into sign-in / home / profile.

--- web/src/home/home.comp.ts
+++ web/src/home/home.comp.ts
@@ -3,11 +3,12 @@ import { NgForm } from '@angular/forms';
 import { Router } from "@angular/router";
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { Project } from '../model/project';
+import { UserService } from '../firebase-service/user.svc';
 
 @Component({
 	selector: 'sl-home',
 	templateUrl: './home.html',
-	providers: []
+	providers: [UserService]
 })
 
 export class HomeComponent implements OnInit {
@@ -15,20 +16,18 @@ export class HomeComponent implements OnInit {
 	items: FirebaseListObservable<any[]>;
 	email: string;
 
-	constructor(af: AngularFire, private router: Router) {
+	constructor(
+		private userService: UserService,
+		af: AngularFire, private router: Router) {
 		this.items = af.database.list('/projects');
 	}
 
 	ngOnInit() {
-		firebase.auth().onAuthStateChanged((user) => {
-			if (user) {
-				console.log(user);
-				this.email = user.email;
-			} else {
-				console.log("No User");
-				this.router.navigate(['sign-in']);
-			}
-		});
+		this.userService.authUser();
+	}
+
+	getUser(){
+		console.log(this.userService.user);
 	}
 
 	addProject(name, status) {
