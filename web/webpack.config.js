dd4951e1353ed5d0af58b3388f712e6ff3759185
Blase, Gary <Gary.Blase@charter.com>
2016-12-21 19:10:52 -0600

    Changed single quotes in webpack config

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,8 +2,8 @@ var ENV = process.env.npm_lifecycle_event;
 var isProd = ENV.indexOf('build') > -1; 
 
 if (!isProd) {
-  module.exports = require(`./config/webpack.dev.js`);
+  module.exports = require('./config/webpack.dev.js');
 }
 else {
-  module.exports = require(`./config/webpack.prod.js`);
+  module.exports = require('./config/webpack.prod.js');
 }
