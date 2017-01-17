503fec653debda4a5565ee7586c36c2a9a0db73c
Ryan Lindbeck <ryanlindbeck@Ryans-MacBook-Pro-2.local>
2017-01-16 19:18:10 -0600

    fixed merge conflicts

--- src/sows/sow-docs/sow-docs.comp.ts
+++ src/sows/sow-docs/sow-docs.comp.ts
@@ -28,61 +28,7 @@ export class SowDocsComponent implements OnInit {
 
     uploadFile() {
         var file = (<HTMLInputElement>document.getElementById("upload")).files[0];
-<<<<<<< HEAD
         this.docUploaderService.upload(file, this.sow);
-    }
-
-    saveSow(sow: any) {
-        let key = sow.$key;
-        if (key) {
-            delete sow.$key;
-            delete sow.$exists;
-            firebase.database().ref('/sows/' + key).update(sow);
-            sow.$key = key;
-=======
-        console.log(file.type);
-        var storageRef = firebase.storage().ref().child(file.name);
-        var uploadTask = storageRef.put(file);
-        uploadTask.then(snapshot => {
-            if (this.sow.documents == null) {
-                this.sow.documents = [];
-            }
-
-            var doc = new Doc();
-            doc.name = file.name;
-            doc.type = this.getFileType(file);
-            doc.downloadURL = snapshot.downloadURL;
-
-            console.log(doc);
-
-            this.sow.documents.push(doc);
-            this.sowsService.saveSow(this.sow);
-            console.log(snapshot.downloadURL);
-
-        });
-
-        uploadTask.on('state_changed', snapshot => {
-            this.progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        },
-            function (error) { console.log(error); }
-        );
-    }
-
-    getFileType(file: File) {
-        switch (file.type) {
-            case "application/pdf":
-                return "pdf";
-            case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-                return "excel"
-            case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-                return "word"
-            case "image/png" || "image/jpg" || "image/jpeg":
-                return "image"
-            default:
-                return "other"
-
->>>>>>> 3d2eb768c9c452a61c18876faf70131644c88a72
-        }
     }
 
 }
--- src/sows/sows.module.ts
+++ src/sows/sows.module.ts
@@ -11,10 +11,7 @@ import { SowDetailsComponent } from './sow-details/sow-details.comp';
 import { SowCommentsComponent } from './sow-comments/sow-comments.comp'
 import { SowDocsComponent } from './sow-docs/sow-docs.comp';
 import { SowsService } from './sows.svc';
-<<<<<<< HEAD
 import { DocUploaderService } from '../doc-uploader/doc-uploader.svc';
-=======
->>>>>>> 3d2eb768c9c452a61c18876faf70131644c88a72
 import {ConfirmDialogComponent} from './sow-details/confirm-dialog.comp';
 
 @NgModule({
@@ -33,10 +30,6 @@ import {ConfirmDialogComponent} from './sow-details/confirm-dialog.comp';
     ConfirmDialogComponent
   ],
   entryComponents: [ConfirmDialogComponent],
-<<<<<<< HEAD
   providers: [SowsService, DocUploaderService]
-=======
-    providers: [SowsService]
->>>>>>> 3d2eb768c9c452a61c18876faf70131644c88a72
 })
 export class SowsModule { }
