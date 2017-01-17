c6a7227d0d71270fe72eb67251c46bf0b0a76625
Carl Shiles <brenan@bot.shakti.local>
2017-01-17 15:43:22 -0600

    Add error handling for mismatched passwords on sign up

--- src/sign-up/sign-up.comp.ts
+++ src/sign-up/sign-up.comp.ts
@@ -7,56 +7,63 @@ import { UserProfile } from '../model/user-profile';
 import { ErrorService } from '../error-service/error.svc'
 
 @Component({
-    selector: 'sl-sign-up',
-    templateUrl: './sign-up.html',
-    providers: [SignUpService]
+	selector: 'sl-sign-up',
+	templateUrl: './sign-up.html',
+	providers: [SignUpService]
 })
 export class SignUpComponent implements OnInit {
-    errorMessage: string;
-    userProfiles: FirebaseListObservable<any[]>;
-    firstName: string;
-    lastName: string;
+	errorMessage: string;
+	userProfiles: FirebaseListObservable<any[]>;
+	firstName: string;
+	lastName: string;
 
-    constructor(
-	private router: Router,
-	private errorService: ErrorService,
-	af: AngularFire) {
-	this.userProfiles = af.database.list('/userProfiles');
-    }
+	constructor(
+		private router: Router,
+		private errorService: ErrorService,
+		af: AngularFire) {
+		this.userProfiles = af.database.list('/userProfiles');
+	}
 
-    ngOnInit() {
-	firebase.auth().onAuthStateChanged((user: firebase.User) => {
-	    if (user) {
-		console.log(user);
-		
-		// Update User Profile info
-		if (this.firstName) {
-		    let up = new UserProfile(this.firstName, this.lastName);
-		    firebase.database().ref('userProfiles/' + user.uid).set(up);
-		    // Send email verification
-		    user.sendEmailVerification();
+	ngOnInit() {
+		firebase.auth().onAuthStateChanged((user: firebase.User) => {
+			if (user) {
+				console.log(user);
+
+				// Update User Profile info
+				if (this.firstName) {
+					let up = new UserProfile(this.firstName, this.lastName);
+					firebase.database().ref('userProfiles/' + user.uid).set(up);
+					// Send email verification
+					user.sendEmailVerification();
+				}
+
+				// Navigate to the home page after user is registered
+				this.router.navigate(['']);
+			} else {
+				console.log("No User");
+			}
+		});
+	}
+
+	signUp(email, password, password2, fname, lname) {
+
+		if (password !== password2) {
+			this.errorService.displayError("Passwords do not match.");
+			return;
 		}
 
-		// Navigate to the home page after user is registered
-		this.router.navigate(['']);
-	    } else {
-		console.log("No User");
-	    }
-	});
-    }
+		this.firstName = fname;
+		this.lastName = lname;
 
-    signUp(email, password, fname, lname) {
-	this.firstName = fname;
-	this.lastName = lname;
-	firebase.auth().createUserWithEmailAndPassword(email, password).catch(error => {
-	    if (error) {
-		console.log(error.message);
-		this.errorService.displayError(error.message);
-	    }
-	});
-    }
+		firebase.auth().createUserWithEmailAndPassword(email, password).catch(error => {
+			if (error) {
+				console.log(error.message);
+				this.errorService.displayError(error.message);
+			}
+		});
+	}
 
-    goTo(route) {
-	this.router.navigate([route]);
-    }
+	goTo(route) {
+		this.router.navigate([route]);
+	}
 }
