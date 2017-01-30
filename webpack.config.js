19f800801561232d0c0db6b54da2dae83c4a191b
Gary Blase <gabx84@gmail.com>
2017-01-30 14:36:12 -0600

    - Changed the build config file

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,5 @@ if (!isProd) {
   module.exports = require('./config/webpack.dev.js');
 }
 else {
-  module.exports = require('./config/webpack.prod.js');
+  module.exports = require('./config/webpack.build.js');
 }
