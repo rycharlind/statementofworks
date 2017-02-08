02889a5f284235896fc0cfb10649b873a06ecc43
Ryan Lindbeck <ryanlindbeck@Ryans-MacBook-Pro-2.local>
2017-02-08 15:18:38 -0600

    added font stuff

---
+++ src/progress-indicator/progress-indicator.comp.ts
@@ -0,0 +1,13 @@
+import { Component, Input, ViewContainerRef } from '@angular/core';
+
+@Component({
+    selector: 'progress-indicator',
+    templateUrl: 'progress-indicator.html'
+})
+
+export class ProgressIndicatorComponent {
+
+    constructor() {
+    }
+
+}
