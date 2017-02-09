3d204a73339bd4795db233ad46f52f5d434ea7aa
Ryan Lindbeck <ryanlindbeck@Ryans-MacBook-Pro-2.local>
2017-02-09 12:51:46 -0600

    Added the ability to edit comments

--- src/sow/sow-comments/sow-comments.comp.ts
+++ src/sow/sow-comments/sow-comments.comp.ts
@@ -27,21 +27,21 @@ export class SowCommentsComponent {
                 this.sow = s;
 
                 this.comments = af.database
-				.list('/sows/' + s.$key + '/comments')
-                .map(comments => {
-                    comments.sort((a : Comment, b : Comment)=> {
-                        var d1 = new Date(a.dateCreated);
-                        var d2 = new Date(b.dateCreated);
-                        if (d1>d2) return -1;
-                        if (d2>d1) return 1;
-                    });
-                    comments.map(c=> {
-                        c.authorUser = af.database.object('/userProfiles/' + c.authorUID);
-                    });
-                    return comments;
-                }) as FirebaseListObservable<any[]>;
-        
-        });
+                    .list('/sows/' + s.$key + '/comments')
+                    .map(comments => {
+                        comments.sort((a: Comment, b: Comment) => {
+                            var d1 = new Date(a.dateCreated);
+                            var d2 = new Date(b.dateCreated);
+                            if (d1 > d2) return -1;
+                            if (d2 > d1) return 1;
+                        });
+                        comments.map(c => {
+                            c.authorUser = af.database.object('/userProfiles/' + c.authorUID);
+                        });
+                        return comments;
+                    }) as FirebaseListObservable<any[]>;
+
+            });
     }
 
     saveComment() {
@@ -50,6 +50,26 @@ export class SowCommentsComponent {
         comment.msg = this.currentComment;
         this.comments.push(comment);
         this.currentComment = '';
+    }
+
+    editComment(comment) {
+        comment.isEditable = true;
+    }
+
+    updateComment(comment) {
+        console.log(comment);
+        let key = comment.$key;
+        if (key) {
+            delete comment.$key;
+            delete comment.$exists;
+            delete comment.authorUser;
+            delete comment.isEditable;
+            firebase.database().ref('/sows/' + this.sow.$key + '/comments/' + key).update(comment);
+            comment.$key = key;
+            comment.isEditable = false;
+        } else {
+            console.log("No key");
+        }
     }
 
     deleteComment(comment) {
